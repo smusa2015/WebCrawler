0be2639f9671ee3e56af1c0c22848b1fdccc4145
ultafa <ultafa@PRINHYLTPDL0530.(none)>
2015-01-29 11:11:10 +0530

    initial commit

---
+++ com/crawler/MailCrawler.java
@@ -0,0 +1,30 @@
+package crawler;
+
+import java.util.*;
+import javax.mail.*;
+
+public class MailCrawler {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty("mail.store.protocol", "imaps");
+        try {
+            Session session = Session.getInstance(props, null);
+            Store store = session.getStore();
+            store.connect("imap.gmail.com", "timepassin2015@gmail.com", "gmailPW@3");
+            Folder inbox = store.getFolder("INBOX");
+            inbox.open(Folder.READ_ONLY);
+            Message msg = inbox.getMessage(inbox.getMessageCount());
+            Address[] in = msg.getFrom();
+            for (Address address : in) {
+                System.out.println("FROM:" + address.toString());
+            }
+            Multipart mp = (Multipart) msg.getContent();
+            BodyPart bp = mp.getBodyPart(0);
+            System.out.println("SENT DATE:" + (msg.getSentDate().getYear()==2014));
+            System.out.println("SUBJECT:" + msg.getSubject());
+            System.out.println("CONTENT:" + bp.getContent());
+        } catch (Exception mex) {
+            mex.printStackTrace();
+        }
+    }
+}
